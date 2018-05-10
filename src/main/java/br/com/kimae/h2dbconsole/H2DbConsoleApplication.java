e4990290d50be40f7234d24f84ce5b49c1817c4c
Escobar <kim.escobar@cwi.com.br>
2018-05-10 09:47:15 -0300

    Init Project.

---
+++ src/main/java/br/com/kimae/h2dbconsole/H2DbConsoleApplication.java
@@ -0,0 +1,12 @@
+package br.com.kimae.h2dbconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class H2DbConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(H2DbConsoleApplication.class, args);
+	}
+}
